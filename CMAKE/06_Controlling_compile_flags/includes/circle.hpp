f797b93c97c636a9cfbe507199638634f6fb4997
Lashi <lashi0812@gmail.com>
2024-01-27 18:00:51 +0000

    control compile flag in cmake

---
+++ CMAKE/06_Controlling_compile_flags/includes/circle.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace geometry {
+namespace area {
+double circle(double radius);
+}
+
+} // namespace geometry
---
+++ CMAKE/06_Controlling_compile_flags/src/main.cpp
@@ -0,0 +1,19 @@
+#include "../includes/circle.hpp"
+#include "../includes/square.hpp"
+
+#include <cstdlib>
+#include <iostream>
+
+int main() {
+    using namespace geometry;
+
+    double radius   = 2.5293;
+    double A_circle = area::circle(radius);
+    std::cout << "A circle of radius " << radius << " has an area of " << A_circle << std::endl;
+
+    double l        = 10.0;
+    double A_square = area::square(l);
+    std::cout << "A square of side " << l << " has an area of " << A_square << std::endl;
+
+    return EXIT_SUCCESS;
+}
